7c1524956b1c955f7a5a8387debe4e2e2944e6de
paco <fjdurios@gmail.com>
2020-03-17 19:54:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hello
         </a>
+	<p>Learn React</p>
       </header>
     </div>
   );
